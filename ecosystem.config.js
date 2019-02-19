6ef6d71fea25bb7603cb6278629cd3f024c80023
Scott Elder <smelder@gmail.com>
2019-02-19 13:54:36 -0600

    deploy prep with PM2 config file

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'nav-service',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-18-224-165-148.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/hello_world.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:selder18/nav-service.git',
+      path: '/home/ubuntu/nav-service',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
